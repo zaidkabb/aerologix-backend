9aea51e353b0811e98abcb7e39a57b3d0ba197c4
stud106708 <stud106708@fh-wedel.de>
2025-12-08 19:24:01 +0100

    first commit

---
+++ src/main/java/org/pm/aerologixbackend/AerologixBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pm.aerologixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerologixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerologixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pm/aerologixbackend/AerologixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pm.aerologixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerologixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
